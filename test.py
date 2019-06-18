4f082d11cc34fe86706b745aadf311626a2f60f0
Alexander Diergarten <alexander.diergarten@cluno.com>
2019-06-18 08:16:01 +0200

    added test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas
+
+a = pd.DataFrame({ 
+ "A": 1.,
+ "B": pd.Timestamp("20130102"),
+ "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+ "D": np.array([3] * 4, dtype="int32"),
+ "E": pd.Categorical(["test", "train", "test", "train"]),
+ "F": "foo"
+ })
