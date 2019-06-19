beb7577d9310d3a08ba555faf1e684594bd2ee21
Alexander Diergarten <alexander.diergarten@cluno.com>
2019-06-19 14:15:43 +0200

    branch 1415

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ a = pd.DataFrame({
  "C": pd.Series(1, index=list(range(4)), dtype="float32"),
  "D": np.array([3] * 4, dtype="int32"),
  "E": pd.Categorical(["test", "train", "test", "train"]),
- "F": "foo"
+ "G": "foo"
  })
