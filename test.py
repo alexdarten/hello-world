1fe613ff4252d2d5fbb1e005f5383f4a22cdb109
Alexander Diergarten <alexander.diergarten@cluno.com>
2019-06-19 14:14:12 +0200

    another comment 1414

--- test.py
+++ test.py
@@ -5,6 +5,5 @@ a = pd.DataFrame({
  "B": pd.Timestamp("20130102"),
  "C": pd.Series(1, index=list(range(4)), dtype="float32"),
  "D": np.array([3] * 4, dtype="int32"),
- "E": pd.Categorical(["test", "train", "test", "train"]),
- "F": "foo"
+ "E": pd.Categorical(["test", "train", "test", "train"])
  })
